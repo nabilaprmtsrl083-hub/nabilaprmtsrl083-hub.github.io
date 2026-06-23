c212b6013dfbd18121a8a74ca874d9679409d123
nabilaprmtsrl083-hub <nabilaprmtsrl083@gmail.com>
2026-06-24 03:27:05 +0700

    Delete app.js

--- app.js
+++
@@ -1,2 +0,0 @@
-document.getElementById('btn').onclick=()=>document.getElementById('projects').scrollIntoView({behavior:'smooth'});
-document.querySelectorAll('.card').forEach(c=>c.onmouseover=()=>c.style.boxShadow='0 20px 40px rgba(0,0,0,.3)');
